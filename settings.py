9263e43373bf439565a9ae46f40936a4ba5146cc
Nikita Volodin <volodin.n@gmail.com>
2022-05-14 17:07:13 -0400

    Support docker secrets with BIND_PASSWORD_FILE

--- settings.py
+++ settings.py
@@ -21,6 +21,11 @@ LOGIN_ATTR = os.environ.get('LOGIN_ATTR', 'uid')
 # You need to secure it otherwise!
 BIND_DN = os.environ.get('BIND_DN')
 BIND_PASSWORD = os.environ.get('BIND_PASSWORD')
+if BIND_PASSWORD is None:
+    PW_FILE = os.environ.get('BIND_PASSWORD_FILE')
+    if PW_FILE is not None:
+        with open(PW_FILE) as file:
+            BIND_PASSWORD = file.read().rstrip('\n')
 
 # Optional user DN pattern string for authentication,
 # e.g. "uid=%s,ou=people,dc=example,dc=com".
