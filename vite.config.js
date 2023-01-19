9bf81e6c62ff9624d2e272e23a777acbfba70bbc
dnknth <dk+git@krachbumm.de>
2023-01-19 11:51:07 +0100

    Bump dependency versions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-const { createVuePlugin } = require('vite-plugin-vue2');
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  plugins: [createVuePlugin()],
+  plugins: [vue()],
 
   base: './',
 
